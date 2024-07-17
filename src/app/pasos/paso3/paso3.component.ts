6e6f2e47133e21a3817dbee32edcc12557fafa7b
Semper Evincere <spereyra.jus@gmail.com>
2024-07-16 21:19:50 -0300

    update semper local

--- src/app/pasos/paso3/paso3.component.ts
+++ src/app/pasos/paso3/paso3.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-paso3',
   templateUrl: './paso3.component.html',
@@ -17,11 +18,25 @@ export class Paso3Component {
   }
 
   downloadPDF(): void {
-    // Lógica para descargar el contenido como PDF
+    const data = document.getElementById('contentToConvert') as HTMLElement;
+    html2canvas(data).then(canvas => {
+      const imgWidth = 208;
+      const pageHeight = 295;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const position = 0;
+
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      
+      pdf.save('resultados.pdf');
+    });
   }
 
   sendEmail(): void {
-    // Lógica para enviar el contenido por correo
+    
   }
 
   mostrarAdvertencia(): void {
