74e0222eed2d4afa816568c52c6375290dae9a95
Semper Evincere <spereyra.jus@gmail.com>
2024-07-29 23:15:44 -0300

    botones popover en español

--- src/app/services/resultado.service.ts
+++ src/app/services/resultado.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Resultados } from '../interfaces/resultados';
 import { ResultadosFrontDTO } from '../interfaces/resultados-front-dto';
 
 @Injectable({
