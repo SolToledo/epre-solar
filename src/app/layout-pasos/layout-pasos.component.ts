fd1d67d2f4c8b90ee5317b838a306c6a36389ca7
Sol <stoledo7@hotmail.com>
2024-08-05 20:25:45 -0300

    paso 3 mas ancho y cards de resultados x3

--- src/app/layout-pasos/layout-pasos.component.ts
+++ src/app/layout-pasos/layout-pasos.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+ /*import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
@@ -19,6 +19,7 @@ import { filter } from 'rxjs';
 export class LayoutPasosComponent implements OnInit {
 
   currentStep: number = 0;
+
   constructor(private route: ActivatedRoute, private router: Router) {
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
@@ -27,6 +28,8 @@ export class LayoutPasosComponent implements OnInit {
       if (childRoute) {
         childRoute.url.subscribe(urlSegments => {
           this.currentStep = Number.parseInt(urlSegments[0]?.path);
+
+
         });
       }
     });
@@ -38,4 +41,56 @@ export class LayoutPasosComponent implements OnInit {
 
   isCollapsed = false;
 
-}
+}*/
+
+
+
+
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'app-layout-pasos',
+  templateUrl: './layout-pasos.component.html',
+  styleUrls: ['./layout-pasos.component.css'],
+  animations: [
+    trigger('menuCollapsed', [
+      state('expand', style({ width: 'visibility: hidden' })),
+      state('collapsed', style({ width: '100%' })),
+      transition('expand <=> collapsed', animate('200ms'))
+    ])
+  ]
+})
+export class LayoutPasosComponent implements OnInit {
+
+  currentStep: number = 0;
+  isPaso3: boolean = false; // Nueva variable para manejar el estado de paso3
+  isCollapsed = false;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      const url = this.router.url;
+      const lastSegment = url.split('/').pop();
+      this.currentStep = Number.parseInt(lastSegment ?? '0', 10);
+      // Actualiza isPaso3 basándote en currentStep
+      this.isPaso3 = this.currentStep === 3;
+
+      // Depuración
+      console.log('Current URL:', url);
+      console.log('Last Segment:', lastSegment);
+      console.log('Current Step:', this.currentStep);
+      console.log('Is Paso3:', this.isPaso3);
+    });
+  }
+
+  ngOnInit(): void {
+    // Inicialización si es necesario
+  }
+} 
+
+
+  
