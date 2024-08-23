56ea1bd99b31631e07965e6aec23aa9379b2a651
Semper Evincere <spereyra.jus@gmail.com>
2024-08-23 19:50:44 -0300

    refactorizacion graficos ts

--- src/app/pasos/paso3/graficos/graficos.component.ts
+++ src/app/pasos/paso3/graficos/graficos.component.ts
@@ -1,30 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Chart, ChartDataset, ChartOptions, ChartType } from 'chart.js';
 import 'chartjs-plugin-datalabels';
+
 @Component({
   selector: 'app-graficos',
   templateUrl: './graficos.component.html',
   styleUrls: ['./graficos.component.css'],
 })
-export class GraficosComponent implements OnInit {
+export class GraficosComponent implements OnInit, AfterViewInit {
   @Input() periodoVeinteanalEmisionesGEIEvitadas!: any[];
   @Input() periodoVeinteanalFlujoEnergia!: any[];
   @Input() periodoVeinteanalFlujoIngresosMonetarios!: any[];
   @Input() periodoVeinteanalGeneracionFotovoltaica!: any[];
   @Input() consumoTotalAnual!: number;
 
+  @ViewChild('emisionesChart') emisionesChartRef!: ElementRef<HTMLCanvasElement>;
+  // @ViewChild('energiaChart') energiaChartRef!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('ingresosChart') ingresosChartRef!: ElementRef<HTMLCanvasElement>;
+  // @ViewChild('fotovoltaicaChart') fotovoltaicaChartRef!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('solarEnergyPieChart') solarEnergyPieChartRef!: ElementRef<HTMLCanvasElement>;
+
   public barChartOptions: ChartOptions = {
     responsive: true,
   };
   public barChartLabels: string[] = [];
-  public barChartType: ChartType = 'bar';
-  public barChartLegend = true;
-  public barChartPlugins = [];
-
-  public barChartData: ChartDataset[] = [];
 
   ngOnInit(): void {
     this.initializeChartLabels();
+   
+  }
+
+  ngAfterViewInit(): void {
     this.loadCharts();
   }
 
@@ -36,243 +42,147 @@ export class GraficosComponent implements OnInit {
 
   loadCharts(): void {
     this.createChart(
-      'emisionesChart',
+      this.emisionesChartRef.nativeElement,
       'line',
-      'Emisiones GEI Evitadas',
-      this.periodoVeinteanalEmisionesGEIEvitadas.map(
-        (item) => item.emisionesTonCO2
-      ),
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(75, 192, 192, 1)'
+      [{
+        label: 'Emisiones GEI Evitadas',
+        data: this.periodoVeinteanalEmisionesGEIEvitadas.map(item => item.emisionesTonCO2),
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+      }]
     );
-
-    this.createChartDouble('energiaChart', 'line', [
-      {
-        label: 'Autoconsumida',
-        data: this.periodoVeinteanalFlujoEnergia.map(
-          (item) => item.autoconsumida
-        ),
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-      },
-      {
-        label: 'Inyectada',
-        data: this.periodoVeinteanalFlujoEnergia.map((item) => item.inyectada),
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)',
-      },
-    ]);
-
-    this.createChartDouble('ingresosChart', 'line', [
-      {
-        label: 'Ahorro Eléctrico Usd',
-        data: this.periodoVeinteanalFlujoIngresosMonetarios.map(
-          (item) => item.ahorroEnElectricidadTotalUsd
-        ),
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-      },
-      {
-        label: 'Ingreso usd por Inyección',
-        data: this.periodoVeinteanalFlujoIngresosMonetarios.map(
-          (item) => item.ingresoPorInyeccionElectricaUsd
-        ),
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)',
-      },
-    ]);
-
+  
     this.createChart(
-      'fotovoltaicaChart',
+      this.ingresosChartRef.nativeElement,
       'line',
-      'Generación Fotovoltaica',
-      this.periodoVeinteanalGeneracionFotovoltaica.map(
-        (item) => item.generacionFotovoltaicaKWh
-      ),
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(153, 102, 255, 1)'
+      [
+        {
+          label: 'Ahorro Eléctrico Usd',
+          data: this.periodoVeinteanalFlujoIngresosMonetarios.map(item => item.ahorroEnElectricidadTotalUsd),
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+        },
+        {
+          label: 'Ingreso usd por Inyección',
+          data: this.periodoVeinteanalFlujoIngresosMonetarios.map(item => item.ingresoPorInyeccionElectricaUsd),
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+        }
+      ]
     );
-
+  
     this.createPieChart();
   }
-
+  
   createChart(
-    elementId: string,
+    canvas: HTMLCanvasElement,
     type: ChartType,
-    label: string,
-    data: number[],
-    backgroundColor: string,
-    borderColor: string
+    datasets: ChartDataset[]
   ): void {
-    const canvas = document.getElementById(elementId) as HTMLCanvasElement;
-    if (canvas) {
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        new Chart(ctx, {
-          type: type,
-          data: {
-            labels: this.barChartLabels,
-            datasets: [
-              {
-                label: label,
-                data: data,
-                borderColor: borderColor,
-                backgroundColor: backgroundColor,
-                pointBackgroundColor: borderColor,
-                pointBorderColor: 'white',
-                pointHoverBackgroundColor: 'white',
-                pointHoverBorderColor: borderColor,
-                fill: true,
-              },
-            ],
-          },
-          options: {
-            responsive: true,
-            scales: {
-              x: {
+    const ctx = canvas.getContext('2d');
+    if (ctx) {
+      new Chart(ctx, {
+        type: type,
+        data: {
+          labels: this.barChartLabels,
+          datasets: datasets.map(dataset => ({
+            ...dataset,
+            fill: true,
+          })),
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              display: true,
+              title: {
                 display: true,
-                title: {
-                  display: true,
-                  text: 'Años',
-                },
+                text: 'Años',
               },
-              y: {
+            },
+            y: {
+              display: true,
+              title: {
                 display: true,
-                title: {
-                  display: true,
-                  text: 'Valor',
-                },
+                text: 'Valor',
               },
             },
           },
-        });
-      } else {
-        console.error('Failed to get 2D context');
-      }
+        },
+      });
     } else {
-      /* console.error('Canvas element not found'); */
-    }
-  }
-
-  createChartDouble(
-    elementId: string,
-    type: ChartType,
-    datasets: Array<{
-      label: string;
-      data: number[];
-      backgroundColor: string;
-      borderColor: string;
-    }>
-  ): void {
-    const canvas = document.getElementById(elementId) as HTMLCanvasElement;
-    if (canvas) {
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        new Chart(ctx, {
-          type: type,
-          data: {
-            labels: this.barChartLabels,
-            datasets: datasets.map((dataset) => ({
-              ...dataset,
-              borderColor: dataset.borderColor,
-              backgroundColor: dataset.backgroundColor,
-              pointBackgroundColor: dataset.borderColor,
-              pointBorderColor: 'white',
-              pointHoverBackgroundColor: 'white',
-              pointHoverBorderColor: dataset.borderColor,
-              fill: true,
-            })),
-          },
-          options: {
-            responsive: true,
-            scales: {
-              x: {
-                display: true,
-                title: {
-                  display: true,
-                  text: 'Años',
-                },
-              },
-              y: {
-                display: true,
-                title: {
-                  display: true,
-                  text: 'Valor',
-                },
-              },
-            },
-          },
-        });
-      } else {
-        console.error('Failed to get 2D context');
-      }
-    } else {
-      /* console.error('Canvas element not found'); */
+      console.error('Failed to get 2D context');
     }
   }
   
+
   createPieChart(): void {
+    if (!this.solarEnergyPieChartRef || !this.solarEnergyPieChartRef.nativeElement) {
+      console.error('Canvas reference is not available');
+      return;
+    }
+  
     const totalGenerado = this.periodoVeinteanalGeneracionFotovoltaica.reduce(
       (acc, item) => acc + item.generacionFotovoltaicaKWh,
       0
     );
   
-    let consumoRestante = totalGenerado - this.consumoTotalAnual ;
+    const consumoRestante = totalGenerado - this.consumoTotalAnual;
   
     const data = {
       labels: ['Energía Solar Generada', 'Consumo Total'],
       datasets: [
         {
           data: [totalGenerado, consumoRestante],
-          backgroundColor: ['rgba(255, 205, 86, 0.7)', 'rgba(201, 203, 207, 0.7)'],
-          hoverBackgroundColor: ['rgba(255, 205, 86, 1)', 'rgba(201, 203, 207, 1)'],
-          borderColor: ['#ffffff', '#ffffff'], // Borde blanco
+          backgroundColor: [
+            'rgba(255, 205, 86, 0.7)',
+            'rgba(201, 203, 207, 0.7)',
+          ],
+          hoverBackgroundColor: [
+            'rgba(255, 205, 86, 1)',
+            'rgba(201, 203, 207, 1)',
+          ],
+          borderColor: ['#ffffff', '#ffffff'],
           borderWidth: 1,
         },
       ],
     };
   
-    const canvas = document.getElementById('solarEnergyPieChart') as HTMLCanvasElement;
-    if (canvas) {
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        new Chart(ctx, {
-          type: 'pie',
-          data: data,
-          options: {
-            responsive: true,
-            animation: {
-              animateScale: true, // Escala la torta
-              animateRotate: true, // Gira la torta al aparecer
-              duration: 1500, // Duración de la animación en milisegundos
-            },
-            interaction: {
-              mode: 'index', // 'point', 'nearest', 'index'
-              intersect: true, // Si es true, solo reacciona cuando el puntero está directamente sobre una sección
-            },
-            maintainAspectRatio: true,
-            aspectRatio: 2,
-            cutout: '10%',
-            plugins: {
-              legend: {
-                display: true,
-                position: 'bottom', // Cambia la posición de la leyenda ('top', 'left', 'bottom', 'right')
-                labels: {
-                  font: {
-                    size: 14, // Tamaño de la fuente
-                  },
-                  color: '#333', // Color de la fuente
+    const ctx = this.solarEnergyPieChartRef.nativeElement.getContext('2d');
+    if (ctx) {
+      new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: {
+          responsive: true,
+          animation: {
+            animateScale: true,
+            animateRotate: true,
+            duration: 1500,
+          },
+          interaction: {
+            mode: 'index',
+            intersect: true,
+          },
+          maintainAspectRatio: true,
+          aspectRatio: 2,
+          cutout: '10%',
+          plugins: {
+            legend: {
+              display: true,
+              position: 'bottom',
+              labels: {
+                font: {
+                  size: 14,
                 },
+                color: '#333',
               },
             },
           },
-        });
-      } else {
-        console.error('Failed to get 2D context');
-      }
+        },
+      });
     } else {
-      console.error('Canvas element not found');
+      console.error('Failed to get 2D context');
     }
-  
   }
 }
