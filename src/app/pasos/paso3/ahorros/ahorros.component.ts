161a69f163612a5a83bc1394c684240c52817107
Semper Evincere <spereyra.jus@gmail.com>
2024-08-24 20:06:28 -0300

    ahorros con valores dinamicos ok

--- src/app/pasos/paso3/ahorros/ahorros.component.ts
+++ src/app/pasos/paso3/ahorros/ahorros.component.ts
@@ -1,25 +1,85 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-ahorros',
   templateUrl: './ahorros.component.html',
-  styleUrls: ['./ahorros.component.css']
+  styleUrls: ['./ahorros.component.css'],
 })
-export class AhorrosComponent {
+export class AhorrosComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+  
+  ahorrosUsdInitial: number = 0;
+  ahorrosUsd: number = 0;
+  panelsCountInitial: number = 0;
+  panelsCountSelected: number = 0;
+  panelCapacityWInitial: number = 0;
+  panelCapacityW: number = 0;
 
-    ahorrosUsd: number = 0;
+  constructor(
+    private sharedService: SharedService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
-    constructor(private sharedService: SharedService) {
+  ngOnInit(): void {
+    this.sharedService.ahorroAnualUsdPromedio$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (ahorroValue) => {
+          if (this.ahorrosUsdInitial === 0) {
+            this.ahorrosUsdInitial = ahorroValue;
+            this.ahorrosUsd = ahorroValue;
+          } else {
+            this.ahorrosUsd = ahorroValue;
+          }
+          this.updateAhorro();  // Actualizamos el ahorro cada vez que cambia el valor inicial
+          this.cdr.detectChanges();
+        },
+      });
 
+    this.sharedService.panelsCountSelected$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (newPanelsCountSelected) => {
+          if (this.panelsCountInitial === 0) {
+            this.panelsCountInitial = newPanelsCountSelected;
+          }
+          this.panelsCountSelected = newPanelsCountSelected;
+          this.updateAhorro();  // Actualizamos el ahorro cada vez que cambia la cantidad de paneles
+          this.cdr.detectChanges();
+        },
+      });
+
+    this.sharedService.panelCapacityW$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (newPanelCapacity) => {
+          if (this.panelCapacityWInitial === 0) {
+            this.panelCapacityWInitial = newPanelCapacity;
+          }
+          this.panelCapacityW = newPanelCapacity;
+          this.updateAhorro();  
+          this.cdr.detectChanges();
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private updateAhorro() {
+    if (this.panelsCountSelected > 0 && this.panelCapacityW > 0 && this.panelsCountInitial > 0 && this.panelCapacityWInitial > 0) {
+      const newAhorroValue =
+        (this.panelsCountSelected * this.panelCapacityW * this.ahorrosUsdInitial) /
+        (this.panelsCountInitial * this.panelCapacityWInitial);
+
+      this.sharedService.setAhorroAnualUsdPromedio(
+        parseInt(newAhorroValue.toFixed(0))
+      );
     }
-
-    ngOnInit(): void {
-      this.sharedService.ahorroAnualUsdPromedio$.subscribe({
-        next: ahorro => {
-          this.ahorrosUsd = parseInt(ahorro.toFixed(0));
-        }
-      })
-      
-    }
+  }
 }
