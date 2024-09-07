095d4fd22c5fd3d738c2b57a5854cbc7814741a1
Semper Evincere <spereyra.jus@gmail.com>
2024-09-07 11:31:07 -0300

    Mantengo solo las nuevas interfaces y actualizo resultados-front-dto y tarifa-intercambio

--- src/app/interfaces/resultados-front-dto.ts
+++ src/app/interfaces/resultados-front-dto.ts
@@ -2,12 +2,14 @@ import { EmisionesGeiEvitadasFront } from "./emisiones-gei-evitadas-front";
 import { FlujoEnergiaFront } from "./flujo-energia-front";
 import { FlujoIngresosMonetariosFront } from "./flujo-ingresos-monetarios-front";
 import { GeneracionFotovoltaicaFront } from "./generacion-fotovoltaica-front";
+import { ParametrosFront } from "./parametros-front";
 import { ProyeccionTarifasFront } from "./proyeccion-tarifas-front";
 import { Resultados } from "./resultados";
 import { SolarDataFront } from "./solar-data-front";
 
 export interface ResultadosFrontDTO {
   solarData: SolarDataFront;
+  parametros?: ParametrosFront,
   periodoVeinteanalGeneracionFotovoltaica: GeneracionFotovoltaicaFront[];
   periodoVeinteanalFlujoEnergia: FlujoEnergiaFront[];
   periodoVeinteanalFlujoIngresosMonetarios: FlujoIngresosMonetariosFront[];
